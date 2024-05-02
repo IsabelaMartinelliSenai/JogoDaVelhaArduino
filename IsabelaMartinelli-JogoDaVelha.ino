bc2aa6b70ad780bd1521348a717634103c07db54
Isabela <163848132+IsabelaMartinelliSenai@users.noreply.github.com>
2024-05-02 15:47:24 -0300

    Arduino

---
+++ IsabelaMartinelli-JogoDaVelha.ino
@@ -0,0 +1,32 @@
+int velha;
+int linha;
+int coluna;
+bool HaVencedor;
+int tabuleiro[9];
+int index;
+for (index = 0; index <= 8; index++){
+  tabuleiro[index] = 0;
+}
+String jogador1 = "Jogador1";
+String jogador2 = "Jogador2";
+String jogada = "";
+int jogadorDaVez = 1;
+
+velha = 1
+do{
+  Serial.println tabuleiro[0] && tabuleiro[1] && tabuleiro[2];
+  Serial.println tabuleiro[3] && tabuleiro[4] && tabuleiro[5];
+  Serial.println tabuleiro[6] && tabuleiro[7] && tabuleiro[8];
+  Serial.println "Digite a posição da sua peça JOGADOR" && jogadorDaVez;
+  digital.Read(jogada);
+  
+}
+
+void setup() {
+ Serial.begin(115200);
+}
+
+void loop() {
+  
+
+}
